49feb00584a732f1c521abfe4936931926aaf774
Kiran Durgesh Nadkarni <kdnadkar@ncsu.edu>
2026-02-05 18:46:36 -0600

    Adding scaffolding generation for admin and output of "rails new" (#15)
    * Adding scaffolding generation for admin and output of "rails new" for new project.
    
    * Deleted github files preventing github PR checks from succeeding
    
    ---------
    
    Co-authored-by: Kiran Nadkarni <kiran.nadk@gmail.com>

---
+++ db/migrate/20260205234622_create_admins.rb
@@ -0,0 +1,13 @@
+class CreateAdmins < ActiveRecord::Migration[8.1]
+  def change
+    create_table :admins do |t|
+      t.integer :id
+      t.string :username
+      t.string :password
+      t.string :name
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
