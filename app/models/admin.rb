50712083b46948dd60d78daa5cb8f3bd22c0e368
Jose Vargas <jvargas6@ncsu.edu>
2026-02-08 21:33:49 -0500

    adding validations for admin (#23)
    - using has_secure_password to encrypt the password
    - ensuring that name, email, password and username are added
    - email is to be unique and ignore case when checking and is validated during create, save and update
    - username and password have minimum settings which are validated on create
    
    Co-authored-by: josev814 <josev814@gmail.com>

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,2 +1,11 @@
 class Admin < ApplicationRecord
+
+  has_secure_password
+
+  validates :name, :presence => true
+  validates :email, :presence => true,
+            :uniqueness => { case_sensitive: false },
+            format: { with: URI::MailTo::EMAIL_REGEXP, on: [:create, :save, :update] }
+  validates :password, :presence => true, length: {minimum: 8}, on: :create
+  validates :username, :presence => true, length: {minimum: 3}, on: :create
 end
