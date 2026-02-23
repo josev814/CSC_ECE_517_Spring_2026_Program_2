08efba135d1552da1af96cf5156f15090a36360d
Jose Vargas <jvargas6@ncsu.edu>
2026-02-22 21:27:18 -0500

    adjusting controller code no longer needed (#107)
    * ensuring admin create new are only available from the console
    
    This is to prevent someone from using http actions to create a new admin
    
    * redirect_to not return_to
    
    * adjusting controller code no longer needed
    
    - with the denial helper we no longer need the redirect_to under new
    - additionally create is empty now since that's strictly a cli method call of save which we seed
    
    ---------
    
    Co-authored-by: josev814 <josev814@gmail.com>

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,5 +1,6 @@
 class AdminsController < ApplicationController
   before_action :admin_access_only, only: %i[ index show edit update ]
+  before_action :block_web_access, only: %i[ new create ]
 
   # GET /admins or /admins.json
   def index
@@ -11,8 +12,6 @@ class AdminsController < ApplicationController
 
   # GET /admins/new
   def new
-    # @admin = Admin.new
-    redirect_to admins_path, alert: 'Access Denied'
   end
 
   # GET /admins/1/edit
@@ -21,17 +20,6 @@ class AdminsController < ApplicationController
 
   # POST /admins or /admins.json
   def create
-    @admin = Admin.new(admin_params)
-
-    respond_to do |format|
-      if @admin.save
-        format.html { redirect_to @admin, notice: "Admin was successfully created." }
-        format.json { render :show, status: :created, location: @admin }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @admin.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # PATCH/PUT /admins/1 or /admins/1.json
@@ -49,8 +37,15 @@ class AdminsController < ApplicationController
 
   private
 
-    # Only allow a list of trusted parameters through.
-    def admin_params
-      params.expect(admin: [ :id, :username, :password, :name, :email ])
+  # Only allow a list of trusted parameters through.
+  def admin_params
+    params.expect(admin: [ :id, :username, :password, :name, :email ])
+  end
+
+  def block_web_access
+    # If the process isn't the console, reject the request
+    unless defined?(Rails::Console)
+      redirect_to root_path, alert: 'Access Denied'
     end
+  end
 end
