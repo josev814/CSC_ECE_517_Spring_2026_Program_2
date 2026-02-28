bc8a41c1e93fa51d09b2b1c6271f7b408a472923
Kiran Durgesh Nadkarni <kdnadkar@ncsu.edu>
2026-02-27 22:35:35 -0600

    Added 'required label' helper for mandatory signup fields. (#125)
    * Added 'required label' helper for mandatory signup fields with a banner that appears at sign-up.
    
    * Change email field to use email_field with required attribute in volunteer form
    
    ---------
    
    Co-authored-by: Kiran Nadkarni <kiran.nadk@gmail.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+    def required_label(form, field, text = nil)
+        text ||=field.to_s.humanize
+        form.label(field) do
+            safe_join([text, content_tag(:span, " *", class: "text-danger")])
+        end
+    end
 end
