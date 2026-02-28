977383450854e9e1352f648fb9d0adc2da2bdf16
DJ Hansen <djhanse2@ncsu.edu>
2026-02-28 18:56:58 -0500

    Made the form a telephone field and added validation for phone number (#134)
    Co-authored-by: Darrin Hansen <darrinjhansen@gmail.com>

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -10,7 +10,8 @@ class Volunteer < ApplicationRecord
     validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
     validates :password, presence: true, on: [ :create ], length: { minimum: 6 }
     validates :full_name, presence: true
-
+    validates :phone_number, format: { with: /\A(\+1\s?)?(\(\d{3}\)|\d{3})[ \-\.]?\d{3}[ \-\.]?\d{4}\z/,
+        message: "must be a valid US phone number" }, allow_blank: true
 
     # Calculation of Total Volunteer Hours
     def total_hours
